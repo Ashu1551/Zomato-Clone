23f9f6ebdf2ebd618b15a2c9d5391137e5448a83
Ashu1551 <a.ashu98dhurve@gmail.com>
2024-03-17 22:56:10 +0530

    Zomato-App

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './Auth/Login'
+import Mymap from './Auth/Mymap'
+import Landing from './Component/Landing'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Mymap/>
+      
+    </div>
+  )
+}
+
+export default App
